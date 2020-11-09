7dcac6ff3f96944c2f4b46d26033564676a56ab8
Faris <redcolony14@example.com>
2020-11-09 23:37:05 +0700

    Update

--- Admin.java
+++ Admin.java
@@ -1,13 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author erallesa
- */
 public class Admin {
+    private String id_admin;
+    private int pass_admin;
+    
+    public Admin(String id_admin, int pass_admin) {
+        this.id_admin = id_admin;
+        this.pass_admin = 0;
+    }
+
+    public String getid_admin() {
+        return this.id_admin;
+    }
+    
+    private int getpass_admin(String id_admin){
+        return this.pass_admin;
+    }
     
 }
